351bff080a08d822c6417875efef6984c0effc92
Deyzze <roman.kushnir.18@gmail.com>
2022-07-27 23:14:46 -0500

    Fist Commit!

---
+++ src/main/java/net/deyzze/learnmod/LearnMod.java
@@ -0,0 +1,14 @@
+package net.deyzze.learnmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LearnMod implements ModInitializer {
+	public static final String MOD_ID = "learnmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
